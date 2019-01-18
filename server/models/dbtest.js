c83b25bb2d814829daa8b42b9cabc35563502377
Daniel Ufeli <danielufeli@gmail.com>
2019-01-18 01:46:38 +0100

    [feature #163177121] heroku fix

--- server/models/dbtest.js
+++ server/models/dbtest.js
@@ -1,4 +1,4 @@
-const dbSchema = `
+ 
   DROP TABLE IF EXISTS meetup CASCADE;
   CREATE TABLE meetup (
     id SERIAL PRIMARY KEY,
@@ -55,5 +55,3 @@ const dbSchema = `
           INSERT INTO meetup(
             location, happeningon, topic, images)
             VALUES ('Lagos', '2019-02-02', 'Welfare', '{"Welfare1"}');
-`;
-export default dbSchema;
